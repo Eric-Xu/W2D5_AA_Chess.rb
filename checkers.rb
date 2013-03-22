3c03dd7454844cfee99bee3e097e6b39d0e9ebe6
Eric-Xu <e.xulander@gmail.com>
2013-03-22 10:34:40 -0700

    First commit

---
+++ checkers.rb
@@ -0,0 +1,26 @@
+class Checkers
+  # interacts with the user
+  # asks each user to make a move until someone wins
+  # print out the board after each move
+end
+
+class Piece
+  # know the valid squares where it can jump to 
+  # know the boundaries of the board
+end
+
+class Board
+  # build the board
+  # populate the board with pieces
+  # show the board
+  # check after each move if all pieces have been removed; aka someone wins
+
+  def initialize
+  	
+  end
+end
+
+class Player
+  # pick a piece
+  # move a piece
+end
